497d3bead768d0e78f7423f0c33cb1099e361f63
James Maynard <jameskirkmaynard@gmail.com>
2017-04-20 15:24:51 -0700

    Sketch out routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
   root to: 'application#angular'
-
+  resources :submissions, only: [:create, :index, :show]
+  resources :poems, only: [:create, :index, :show] do
+    resources :submissions, only: [:show]
+  end
+  resources :journals, only: [:create, :index, :show] do
+    resources :submissions, only: [:show]
+  end
 end
