3973f625f9ccfac421bd9d8b68de2d676913c228
James <james.maynard@modop.com>
2017-09-21 08:45:02 -0700

    code cleanup for app.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,62 +3,74 @@ angular.module('submissionsHub', ['ui.router', 'templates'])
   '$stateProvider',
   '$urlRouterProvider',
   function($stateProvider, $urlRouterProvider) {
-    $stateProvider.state('home', {
-      url: '/home',
-      templateUrl: 'partials/_home.html',
-      controller: 'HomeCtrl',
-      resolve: {
-        promise: ['api', function(api) {
-          for (key in api) {
-            if (typeof api[key] === "object") {
-              api.getAll(key);
-            }
-          };
-        }]}
-    });
-    $stateProvider.state('poems', {
-      url: '/poems/{id}',
-      templateUrl: 'partials/_poems.html',
-      controller: 'PoemsCtrl',
-      resolve: {
-        poem: ['$stateParams', 'api', function($stateParams, api) {
-          return api.get('poems', $stateParams.id);
-        }]
-      }
-    });
-    $stateProvider.state('submissions', {
-      url: '/submissions/{id}',
-      templateUrl: 'partials/_submissions.html',
-      controller: 'SubmissionsCtrl',
-      resolve: {
-        submission: ['$stateParams', 'api', function($stateParams, api) {
-          return api.get('submissions', $stateParams.id);
-        }]
-      }
-    });
-    $stateProvider.state('journals', {
-      url: '/journals/{id}',
-      templateUrl: 'partials/_journals.html',
-      controller: 'JournalsCtrl',
-      resolve: {
-        journal: ['$stateParams', 'api', function($stateParams, api) {
-          return api.get('journals', $stateParams.id)
-        }]
-      }
-    });
-    $stateProvider.state('new-submission', {
-      url: '/new-submission',
-      templateUrl: 'partials/_new-submission.html',
-      controller: 'HomeCtrl',
-      resolve: {
-        promise: ['api', function(api) {
-          for (key in api) {
-            if (typeof api[key] === "object") {
-              api.getAll(key);
-            }
-          };
-        }]}
-    });
-    $urlRouterProvider.otherwise('home');
-  }
+
+      $stateProvider.state('home', {
+          url: '/home',
+          templateUrl: 'partials/_home.html',
+          controller: 'HomeCtrl',
+          resolve: {
+              promise: ['api', function(api) {
+
+                  for (key in api) {
+                      if (typeof api[key] === "object") {
+                          api.getAll(key);
+                      }
+                  };
+                  
+              }]
+          }
+      });
+
+      $stateProvider.state('poems', {
+          url: '/poems/{id}',
+          templateUrl: 'partials/_poems.html',
+          controller: 'PoemsCtrl',
+          resolve: {
+              poem: ['$stateParams', 'api', function($stateParams, api) {
+                  return api.get('poems', $stateParams.id);
+              }]
+          }
+      });
+
+      $stateProvider.state('submissions', {
+          url: '/submissions/{id}',
+          templateUrl: 'partials/_submissions.html',
+          controller: 'SubmissionsCtrl',
+          resolve: {
+              submission: ['$stateParams', 'api', function($stateParams, api) {
+                  return api.get('submissions', $stateParams.id);
+              }]
+          }
+      });
+
+      $stateProvider.state('journals', {
+          url: '/journals/{id}',
+          templateUrl: 'partials/_journals.html',
+          controller: 'JournalsCtrl',
+          resolve: {
+              journal: ['$stateParams', 'api', function($stateParams, api) {
+                  return api.get('journals', $stateParams.id)
+              }]
+          }
+      });
+
+      $stateProvider.state('new-submission', {
+          url: '/new-submission',
+          templateUrl: 'partials/_new-submission.html',
+          controller: 'HomeCtrl',
+          resolve: {
+              promise: ['api', function(api) {
+
+                  for (key in api) {
+                      if (typeof api[key] === "object") {
+                          api.getAll(key);
+                      }
+                  };
+
+              }]
+          }
+      });
+
+      $urlRouterProvider.otherwise('home');
+    }
 ])
