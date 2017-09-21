2f26c947fef391defea54e2a64b4b63fc5efeacb
James <james.maynard@modop.com>
2017-09-21 08:47:42 -0700

    code cleaup for submissionsCtrl.js

--- app/assets/javascripts/controllers/submissionsCtrl.js
+++ app/assets/javascripts/controllers/submissionsCtrl.js
@@ -1,13 +1,16 @@
 angular.module('submissionsHub')
 .controller('SubmissionsCtrl', [
-  '$scope',
-  'api',
-  'submission',
-  function($scope, api, submission) {
-    $scope.submission = submission.submissions;
-    $scope.poems = submission.poems
-    api.getJournal(submission.journal_id).then(function(res) {
-      $scope.journal = res.data[0];
-    })
-  }
-])
+    '$scope',
+    'api',
+    'submission',
+
+    function($scope, api, submission) {
+        $scope.submission = submission.submissions;
+        $scope.poems = submission.poems
+        
+        api.getJournal(submission.journal_id).then(function(res) {
+            $scope.journal = res.data[0];
+        })
+    }
+]
+);
