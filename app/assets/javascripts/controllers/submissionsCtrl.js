ff502989592ba0e17a5313b24ff6091c3299a67d
James <james.maynard@modop.com>
2017-09-21 10:40:32 -0700

    Add accepted binding and function in submissionsCtrl and partial, cleanup api.js

--- app/assets/javascripts/controllers/submissionsCtrl.js
+++ app/assets/javascripts/controllers/submissionsCtrl.js
@@ -6,11 +6,36 @@ angular.module('submissionsHub')
 
     function($scope, api, submission) {
         $scope.submission = submission.submissions;
-        $scope.poems = submission.poems
-        
+        $scope.poems = submission.poems;
+
+        /***** controller functions *****/
+
+        let getAcceptedValues = function() {
+          console.log('inside the getAcceptedValues function');
+          $scope.poems.forEach(function(poem) {
+            poem.accepted === true ? poem.accepted_value = "accepted" : poem.accepted_value = "not accepted";
+          });
+
+        }
+
+        /***** _submissions.html scoped functions *****/
+
+        $scope.poemAccepted = function(index) {
+            let poem = $scope.poems[index];
+
+            poem.accepted = true;
+            poem.journal_accepted = $scope.journal.title
+            getAcceptedValues();
+
+        };
+
+        /***** on _submission.html load *****/
+
         api.getJournal(submission.journal_id).then(function(res) {
-            $scope.journal = res.data[0];
-        })
+          $scope.journal = res.data[0];
+        });
+
+        getAcceptedValues();
+
     }
-]
-);
+]);
--- app/assets/javascripts/services/api.js
+++ app/assets/javascripts/services/api.js
@@ -1,49 +1,60 @@
 angular.module('submissionsHub')
 .factory('api', [
-  '$http',
-  function($http) {
-    var obj = {
-      poems: [],
-      submissions: [],
-      journals: []
-    };
-    obj.getAll = function(model) {
-      $http.get('/'+ model + '.json').then(function(data) {
-        angular.copy(data.data, obj[model]);
-      });
-    };
-    obj.get = function(model, id) {
-      return $http.get('/' + model + '/' + id + '.json').then(function(data) {
-        if (model === "journals") {
-          data.data[0].submissions = data.data[1];
-          return data.data[0];
-        } else {
-          model === "poems" ? data.data[0].submissions = data.data[1] : data.data[0].poems = data.data[1]
-          return data.data[0];
+    '$http',
+    function($http) {
+        var obj = { poems: [], submissions: [], journals: [] };
+
+        obj.getAll = function(model) {
+            $http.get('/'+ model + '.json').then(function(data) {
+                angular.copy(data.data, obj[model]);
+            });
+        };
+
+        obj.get = function(model, id) {
+              return $http.get('/' + model + '/' + id + '.json').then(function(data) {
+                if (model === "journals") {
+                    data.data[0].submissions = data.data[1];
+                    return data.data[0];
+                } else {
+                    model === "poems" ? data.data[0].submissions = data.data[1] : data.data[0].poems = data.data[1]
+                    return data.data[0];
+                }
+            });
+        };
+
+        /***** journal REST *****/
+
+        obj.getJournal = function(id) {
+            return $http.get('/journals/' + id + '.json');
+        };
+
+        obj.createJournal = function(journal) {
+            return $http.post('/journals.json', journal).then(function(data) {
+                obj.journals.push(data.data);
+            });
         }
-      });
-    };
-    obj.getJournal = function(id) {
-      return $http.get('/journals/' + id + '.json');
-    };
-    obj.getPoems = function(submission_id) {
-      $http.get('/poems.json');
-    };
-    obj.createPoem = function(poem) {
-      return $http.post('/poems.json', poem).then(function(data) {
-        obj.poems.push(data.data);
-      });
-    };
-    obj.createSubmission = function(submission) {
-      return $http.post('/submissions.json', submission).then(function(data) {
-        obj.submissions.push(data.data);
-      })
-    };
-    obj.createJournal = function(journal) {
-      return $http.post('/journals.json', journal).then(function(data) {
-        obj.journals.push(data.data);
-      })
+
+        /***** poem REST *****/
+
+        obj.getPoems = function(submission_id) {
+            $http.get('/poems.json');
+        };
+
+        obj.createPoem = function(poem) {
+            return $http.post('/poems.json', poem).then(function(data) {
+                obj.poems.push(data.data);
+            });
+        };
+
+        /***** submission REST *****/
+
+        obj.createSubmission = function(submission) {
+            return $http.post('/submissions.json', submission).then(function(data) {
+                obj.submissions.push(data.data);
+            })
+        };
+
+
+        return obj;
     }
-    return obj;
-  }
 ]);
